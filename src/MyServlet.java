12485b9ca9efafb5a184d7b61a465dd203034ec2
Yevgeniy <archangel.inside.me@gmail.com>
2017-04-02 22:17:48 +0600

    1

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,15 +1,19 @@
+import test.DAO;
+
 import java.io.IOException;
 
 /**
  * Created by Yevgeniy on 02.04.2017.
  */
-@javax.servlet.annotation.WebServlet(name = "MyServlet", urlPatterns = "/test")
+@javax.servlet.annotation.WebServlet(name = "MyServlet", urlPatterns = "/posts")
 public class MyServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setAttribute("posts", DAO.getPosts());
+        request.getRequestDispatcher("WEB-INF/posts.jsp").forward(request, response);
         response.getOutputStream().write("THAT IS TEST TOO".getBytes());
     }
 }
--- src/test/DAO.java
+++ src/test/DAO.java
@@ -7,8 +7,9 @@ import java.util.List;
  * Created by Yevgeniy on 02.04.2017.
  */
 public class DAO {
+    static List<Post> posts;
     static {
-        List<Post> posts = new ArrayList<Post>();
+        posts = new ArrayList<Post>();
         posts.add(new Post(1, "hello"));
         posts.add(new Post(2, "everything"));
         posts.add(new Post(3, "burn"));
