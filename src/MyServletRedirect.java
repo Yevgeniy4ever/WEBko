4e6e5fcea0fb01695d15117675aba101a1b5b4ca
Yevgeniy <archangel.inside.me@gmail.com>
2017-04-03 09:30:24 +0600

    2

--- src/MyServletRedirect.java
+++ src/MyServletRedirect.java
@@ -5,15 +5,19 @@ import java.io.IOException;
 /**
  * Created by Yevgeniy on 02.04.2017.
  */
-@javax.servlet.annotation.WebServlet(name = "MyServlet", urlPatterns = "/posts")
-public class MyServlet extends javax.servlet.http.HttpServlet {
+@javax.servlet.annotation.WebServlet(name = "MyServletRedirect", urlPatterns = "/redirect")
+public class MyServletRedirect extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         request.setAttribute("posts", DAO.getPosts());
+        response.sendRedirect("http://ya.ru");
+
+
+        /*        request.setAttribute("posts", DAO.getPosts());
         request.getRequestDispatcher("WEB-INF/posts.jsp").forward(request, response);
-/*        response.getOutputStream().write("THAT IS TEST TOO".getBytes());*/
+        response.getOutputStream().write("THAT IS TEST TOO".getBytes());*/
     }
 }
--- web/WEB-INF/posts.jsp
+++ web/WEB-INF/posts.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: Yevgeniy
@@ -11,6 +12,13 @@
     <title>Guestbook posts</title>
 </head>
 <body>
-
+    <table border="1">
+        <c:forEach items="${requestScope.posts}" var="post">
+        <tr>
+            <td>${post.id}</td>
+            <td>${post.txt}</td>
+        </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
